9249bbd3d9139c8d7f4a3236f58fb28cccd68d83
unknown <softech.series@gmail.com>
2023-10-24 03:47:21 -0500

    Base de datos

--- database.js
+++ database.js
@@ -5,8 +5,8 @@ const { Sequelize } = require('sequelize');
 // Conexión a la base de datos MySQL
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
 });
 
 sequelize.authenticate()
