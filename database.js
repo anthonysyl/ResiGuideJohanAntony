a75b30d705bcdcbddb9c01e141ab9927c3bbf3ee
unknown <softech.series@gmail.com>
2023-10-24 04:22:32 -0500

    F

--- database.js
+++ database.js
@@ -6,7 +6,6 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT
 });
 
 sequelize.authenticate()
